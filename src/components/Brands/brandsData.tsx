c200ad2b9e498ad864a4f5f897abcbe3da2ed609
Hecze <hectorzerrillo85@gmail.com>
2024-05-04 17:29:15 -0500

    first commit

---
+++ src/components/Brands/brandsData.tsx
@@ -0,0 +1,48 @@
+import { Brand } from "@/types/brand";
+
+const brandsData: Brand[] = [
+  {
+    id: 6,
+    name: "America",
+    href: "www.latamairlines.com",
+    image: "/images/brands/america.svg",
+    imageLight: "/images/brands/america.svg",
+  },
+  {
+    id: 1,
+    name: "covidien",
+    href: "https://hpmedical.com.bo/",
+    image: "/images/brands/covidien.svg",
+    imageLight: "/images/brands/covidien.svg",
+  },
+  {
+    id: 2,
+    name: "farmacias-yza",
+    href: "https://www.yza.mx/",
+    image: "/images/brands/farmacias-yza.svg",
+    imageLight: "/images/brands/farmacias-yza.svg",
+  },
+  {
+    id: 3,
+    name: "google",
+    href: "https://google.com",
+    image: "/images/brands/google.svg",
+    imageLight: "/images/brands/google.svg",
+  },
+  {
+    id: 4,
+    name: "amazon",
+    href: "https://amazon.com",
+    image: "/images/brands/amazon.svg",
+    imageLight: "/images/brands/amazon.svg",
+  },
+  {
+    id: 5,
+    name: "PlainAdmin",
+    href: "https://plainadmin.com",
+    image: "/images/brands/plainadmin.svg",
+    imageLight: "/images/brands/plainadmin-light.svg",
+  },
+];
+
+export default brandsData;
