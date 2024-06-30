14caaf0ffbe2416a0b84ae377586b0d33c121b6b
Hecze <hectorzerrillo85@gmail.com>
2024-06-30 16:01:26 -0500

    update next config para no buguearse

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   //output: "export",
   //trailingSlash: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '3.128.94.171',
+        port: '4500',
+        pathname: '/**',
+      },
+    ],
     domains: ['3.128.94.171'],
   },
 };
